298fac4c854b9e59092fe97fdcfa04b9a052d845
Xenia Kustanova <xenia.kustanova@gmail.com>
2018-10-01 23:08:49 +0200

    add temporary version

--- 02-objects_classes/scorelib.py
+++ 02-objects_classes/scorelib.py
@@ -1,46 +1,157 @@
-#Print,Edition,Composition,Voice,Person
-path = "scorelib.txt"
+import re
 
-
-class Person:
-    def __init__(self, name, born, died):
-        self.name =  name
+class Person(object):
+    def __init__(self, name, born, died, type):
+        self.name = name
         self.born = born
-        self.died =  died
+        self.died = died
+        self.type = type
+
+    def __repr__(self):
+        return "Name: {0}, Born: {1}, Died: {2}".format(self.name, self.born, self.died)
 
 
-class Voice:
-    def __init__(self, name, range):
-        self.name =  name
-        self. range =  range
+class Editor(Person):
+    def __init__(self, name=None, born=None, died=None):
+        Person.__init__(self, name, born, died, "editor")
 
 
-class Composition:
-    def __init__(self, name, incipit, key, genre, year, voices, authors):
+class Composer(Person):
+    def __init__(self, name=None, born=None, died=None):
+        Person.__init__(self, name, born, died, "composer")
+
+    def create_from_text(self, text_composer):
+        tmp_name, *rest = text_composer.split("(")
+        self.name = tmp_name.strip()
+
+        # normal range
+        parsed = re.search("(\d\d\d\d--\d\d\d\d)", text_composer)
+        if parsed:
+            b, d = parsed.group().split("--")
+            self.born, self.died = int(b), int(d)
+
+        # born start with *
+        parsed = re.search("\*\d\d\d\d", text_composer)
+        if parsed:
+            self.born = int(parsed.group()[1:6])
+
+        # died start with +
+        parsed = re.search("\+\d\d\d\d", text_composer)
+        if parsed:
+            self.died = int(parsed.group()[1:6])
+
+
+
+class Voice(object):
+    def __init__(self, name=None, range=None):
+        self.name = name
+        self.range = range
+
+    def __repr__(self):
+        return "Name: {0}, Range: {1}".format(self.name, self.range)
+
+    def create_from_text(self, text_voice):
+        if "--" in text_voice:
+            temp_range, *temp_name = text_voice.split(",")
+            self.name = "".join(temp_name).strip()
+            self.range = temp_range.strip()
+        else:
+            self.name = text_voice.strip()
+
+
+class Composition(object):
+    def __init__(self, name=None, incipit=None, key=None, genre=None, year=None, voices=None, authors=None):
         self.name = name
         self.incipit = incipit
         self.key = key
         self.genre = genre
         self.year = year
-        self.voices = voices
-        self.authors = authors
+        self.voices = voices or []
+        self.authors = authors or []
+
+    def __repr__(self):
+        return "Name: {0}, Incipit: {1}, Key: {2}, Genre: {3}, Year: {4}, Voices: {5}, Authors: {6}".format(
+            self.name, self.incipit, self.key, self.genre, self.year, self.voices, self.authors
+        )
+
+    def set_composers(self, text_value):
+        for c in text_value.split(";"):
+            composer = Composer()
+            composer.create_from_text(c.strip())
+            self.authors.append(composer)
+
+    def set_name(self, text_value):
+        self.name = text_value
+
+    def set_genre(self, text_value):
+        self.genre = text_value
+
+    def set_key(self, text_value):
+        self.key = text_value
+
+    def set_year(self, text_value):
+        try:
+            if int(text_value) > 999 and int(text_value) < 10000:
+                self.year = text_value
+            else:
+                raise ValueError
+        except ValueError:
+            self.year = None
+
+    def set_incipit(self, text_value):
+        self.incipit = text_value
+
+    def add_voice(self, text_value):
+        voice = Voice()
+        voice.create_from_text(text_value)
+        self.voices.append(voice)
+
+    @staticmethod
+    def _clean_composer(raw_name):
+        only_name = raw_name.split("(")[0].strip()
+        surname, *rest = only_name.split(" ")
+        initials = ""
+        for name in rest:
+            if name.strip()[-1] == ".":
+                initials += name
+            else:
+                initials += "{0}.".format(name[0])
+        return "{0} {1}".format(surname, initials)
 
 
-
-class Edition:
-    def __init__(self, composition, authors, name):
-        self.composition = composition
-        self.authors = authors
+class Edition(object):
+    def __init__(self, composition=None, authors=None, name=None):
+        self.composition = composition or self._create_default_composition()
+        self.authors = authors or []
         self.name = name
 
+    def __repr__(self):
+        return "Name: {0}, Authors: {1}, Composition: {2}".format(self.name, self.authors, self.composition)
 
-class Print:
-    def __init__(self, edition, print_id, partiture):
-        self.edition = edition
+    @staticmethod
+    def _create_default_composition():
+        return Composition()
+
+
+class Print(object):
+    def __init__(self, print_id, edition=None, partiture=None):
         self.print_id = print_id
+        self.edition = edition or self._create_default_edition()
         self.partiture = partiture
 
+    @staticmethod
+    def _create_default_edition():
+        return Edition()
 
+    def format(self):
+        print("""Print ID: {0}, Edition: {1}, Partiture: {2}""".format(
+            self.print_id, self.edition, self.partiture)
+        )
 
-def load(filename):
-    pass
+    def set_partiture_from_text(self, text_value):
+        if text_value.strip() == "yes":
+            self.partiture = True
+        elif text_value.strip() == "no":
+            self.partiture = False
+        else:
+            self.partiture = None
--- 02-objects_classes/test.py
+++ 02-objects_classes/test.py
@@ -1,3 +1,123 @@
+from scorelib import Print
+import re
+import sys
 
-if __name__ == '__main__':
-    print(1)
+FILE_PATH = "scorelib.txt"
+
+
+def parse_text(pattern, line):
+    parsed_line = re.match(pattern, line)
+    if parsed_line:
+        parsed_text = parsed_line.group(2)
+        parsed_text = parsed_text.strip() if parsed_text else None
+        if parsed_text == "":
+            return None
+        return parsed_text
+
+
+def parse_line(line):
+    patterns = {
+        "print": re.compile("(Print Number: )(.*)"),
+        "composer": re.compile("(Composer: )(.*)"),
+        "title": re.compile("(Title: )(.*)"),
+        "genre": re.compile("(Genre: )(.*)"),
+        "key": re.compile("(Key: )(.*)"),
+        "composition_year": re.compile("(Composition Year: )(.*)"),
+        "edition": re.compile("(Edition: )(.*)"),
+        "editor": re.compile("(Editor: )(.*)"),
+        "voice": re.compile("(Voice \d: )(.*)"),
+        "partiture": re.compile("(Partiture: )(.*)"),
+        "incipit": re.compile("(Incipit: )(.*)"),
+    }
+
+    if line == "\n":
+        return {
+            "type": "newline",
+            "value": None,
+        }
+
+    for line_type, pattern in patterns.items():
+        parsed_line = parse_text(pattern, line)
+        if parsed_line:
+            return {
+                "type": line_type,
+                "value": parsed_line,
+            }
+
+
+def load(file_path):
+    prints = []
+
+    with open(file_path, "r") as file:
+
+        p = None
+
+        for line in file:
+
+            parsed = parse_line(line)
+
+            if not parsed:
+                continue
+
+            parsed_type = parsed["type"]
+            parsed_value = parsed["value"]
+
+            if parsed_type == "print":
+                p = Print(parsed_value)
+                prints.append(p)
+
+            if parsed_type == "composer":
+                p.edition.composition.set_composers(parsed_value)
+
+            if parsed_type == "title":
+                p.edition.composition.set_name(parsed_value)
+
+            if parsed_type == "genre":
+                p.edition.composition.set_genre(parsed_value)
+
+            if parsed_type == "key":
+                p.edition.composition.set_key(parsed_value)
+
+            if parsed_type == "composition_year":
+                p.edition.composition.set_year(parsed_value)
+
+            if parsed_type == "edition":
+                pass
+
+            if parsed_type == "editor":
+                pass
+
+            if parsed_type == "voice":
+                p.edition.composition.add_voice(parsed_value)
+
+            if parsed_type == "partiture":
+                p.set_partiture_from_text(parsed_value)
+
+            if parsed_type == "incipit":
+                p.edition.composition.set_incipit(parsed_value)
+
+            # just to be sure
+            if parsed_type == "newline":
+                p = None
+
+        file.close()
+
+    return prints
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("First argument should be path to library file!")
+
+    # filename = sys.argv[1]  # TODO: uncomment
+    filename = FILE_PATH
+
+    try:
+        list_of_prints = load(filename)
+
+        for print_object in list_of_prints[:20]:
+            print_object.format()
+            # print()
+
+    except FileNotFoundError:
+        print("Bad path to library file")
